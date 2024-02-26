057dbf705b0d8db1e78d103f4a6e5d7ca23eb61b
rnzbobi <rhenzbhovieviloria@gmail.com>
2024-02-26 16:22:38 +0800

    Update login.php

--- html/login.php
+++ html/login.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Signup</title>
+    <title>Login</title>
     <link rel="stylesheet" href="css/signupstyle.css" type="text/css" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
