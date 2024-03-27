ba5da14ac2756471a7a1350fccd42f3a946035a6
cosmicElze <charlemagne_elliot_craw@dlsu.edu.ph>
2024-03-27 19:08:08 +0800

    Placed all CSS onto one file, and also updated the Balance so it reflects the actual balance of the user on view_cart.php

--- html/view_cart.php
+++ html/view_cart.php
@@ -17,139 +17,9 @@ if (!isset($_SESSION['username'])) {
     <link rel="stylesheet" href="css/style.css" type="text/css" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;1,200&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
-    <style>
-        .button{
-            display: flex;
-            background: white;
-            border: 0px;
-        }
-        .button-bottom{
-            border:3px solid black;
-            background-color:white;
-            color:black;
-            font-size:20px;
-            font-family: 'Open Sans', sans-serif;  
-            padding: 10px 20px;
-            margin: 0;
-            max-height: 53px;
-        }
-        .button-bottom:hover{
-            border:3px solid black;
-            border-color:black;
-            background-color:black;
-            color:white; 
-        }
-        .bottom{
-            display: flex;
-            flex-direction: row;
-            justify-content: flex-start;
-            gap:20px;
-        }
-        .cart{
-            display: flex;
-            font-family: 'Open Sans', sans-serif;  
-            font-size: 15px;      
-            line-height: 1.4;
-            flex-direction: column; 
-            align-items: center;
-        }
-        .cart-item-holder{
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            padding: 20px;
-            width: 40%;  
-        }
-        .cart-item-details{
-            display:flex;
-            flex: 1;
-            flex-direction:column;
-            margin-right: 20px;
-        }
-        .cart-item-name{
-            display: flex;
-            flex-direction: row;
-            text-align: left;
-            font-weight: bold;
-            font-size: 20px;    
-        }
-        .cart-item-placeholder{
-            display:block;
-            flex-direction:row;
-            text-align: left;
-            line-height: 1.2;
-        }
-        .cart-item-delete{
-            display:block;
-            flex-direction:row;
-            text-align: right;
-            margin-left: 17px; 
-            margin-top: 12px;
-            line-height: 1.2;
-            margin-bottom:20px;
-        }
-        .cart-image-holder{
-            display: flex;
-            flex-direction: column;
-            margin-right: 20px; 
-        }
-        .top{
-            display: flex;
-            font-family: 'Open Sans', sans-serif;  
-            font-weight: bold;
-            font-size: 30px;
-            text-align: center; 
-            justify-content: center;
-            flex-direction: column;
-            margin-bottom: 50px;
-        }
-        .amountitems{
-            display: flex;
-            padding:5px;
-            font-size:15px;
-            text-align: center; 
-            justify-content: center;
-        }
-        .editquantity{
-            display: flex;
-            height: 50px;
-            min-width: 80px;
-            border: 2px solid black;
-            align-items: center;
-            justify-content: center;
-        }
-        .minusplus{
-            width: 100%;
-            background:white;
-            border: white;
-            font-size: 20px; 
-        }
-        .editquantity span.count{
-            font-size:20px;
-        } 
-        .total-price-item{
-            display: flex;
-            text-align: right;
-            font-size:20px;
-            margin-left:40px;
-            margin-bottom:20px;
-            min-width:100px;
-            align-items: center;
-            justify-content: center;
-        }
-        .separator{
-            justify-content: left;
-            flex-direction:column;
-            font-size:20px;
-        }
-        .link-design{
-            text-decoration: none;
-            color: inherit;
-            margin: 0;
-        }
-    </style>
 </head>
 <body>
+
 <header>
         <div class="header">
             <div class="logo">
@@ -157,8 +27,8 @@ if (!isset($_SESSION['username'])) {
                     <img src="images/logo.png" alt="Logo">
                 </a>
             </div>
-            <form class="search-form" action="index.php" method="GET">
-                <input type="text" name="search" class="search-input" placeholder="Search...">
+            <form class="search-form" action="index.php?search=" method="GET">
+                <input type="text" name="search" class="search-input" placeholder="Search..." value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
                 <button type="submit" class="search-button"><img src="images/search-interface-symbol.png" alt="Search"></button>
             </form>
             <div class="nav-links">
@@ -173,16 +43,25 @@ if (!isset($_SESSION['username'])) {
                 echo '<a href="login.php"><h2><span id="user-id">Login/Signup</span></h2></a>';
             }
             ?>
-
-            </div>
-            <div class="Balance-header">
-                <h2 id="balance">Balance: <span id="balance-value">10000</span></h2>
+                <h2>Balance: <span id="balance-value"><?php 
+                $getBalance = mysqli_query($conn,
+                    "SELECT user_id.balance AS 'balanceUser'
+                    FROM user_id 
+                        JOIN carts ON user_id.userid = carts.user_id
+                        JOIN individual_clothes ON carts.item_id = individual_clothes.id
+                    WHERE user_id.username='$username'");
+                if ($getBalance && mysqli_num_rows($getBalance) > 0){
+                    $balanceRow = mysqli_fetch_assoc($getBalance);
+                    $balance = $balanceRow['balanceUser'];
+                    echo $balance;
+                }
+                else{
+                    echo 0;
+                }?></span></h2>
             </div>
         </div>
-    </header>  
+    </header>
 <?php
-    
-
     $initializeCart = mysqli_query($conn,
     "SELECT *, SUM(c.quantity) AS total_quantity
     FROM user_id ui 
@@ -233,9 +112,9 @@ if (!isset($_SESSION['username'])) {
     
 ?>
 <center>
-<div class="top">
+<div class="view_cart-top">
     Your Shopping Cart
-        <div class="amountitems">
+        <div class="view_cart-amountitems">
             Total items: 
             <?php
                 $countItemsQuery = mysqli_query($conn,
@@ -251,14 +130,14 @@ if (!isset($_SESSION['username'])) {
             ?>
         </div>
 </div>
-<div class="cart">
+<div class="view_cart">
 <?php
     $subTotal=0;
     while($viewCart=mysqli_fetch_assoc($getuserCart)){
         echo "<div class='cart-item-holder'>";
 
             echo "<div class = 'cart-image-holder'>";
-                echo "<a class='link-design' href='view.php?item_id=".$viewCart['id']."'>";
+                echo "<a class='link-design-view_cart' href='view.php?item_id=".$viewCart['id']."'>";
                     echo "<img src='".$viewCart['image_URL']."'style='width: 100px; height: 100px;'>";
                 echo "</a>";
             echo "</div>";
@@ -266,7 +145,7 @@ if (!isset($_SESSION['username'])) {
                 echo "<div class='cart-item-details'>";
 
                     echo "<div class = 'cart-item-name'>";
-                        echo "<a class='link-design' href='view.php?item_id=".$viewCart['id']."'>";
+                        echo "<a class='link-design-view_cart' href='view.php?item_id=".$viewCart['id']."'>";
                             echo $viewCart['name'];
                         echo "</a>";
                     echo "</div>";
@@ -282,24 +161,24 @@ if (!isset($_SESSION['username'])) {
                     
                 echo "</div>";
                 echo "<form action='update_quantity.php' method='GET'>";
-                    echo "<div class ='editquantity' id='editquantity".$viewCart['id']."'>";
+                    echo "<div class ='view_cart-editquantity' id='editquantity".$viewCart['id']."'>";
                         echo "<input type='hidden' name='item_id' value='".$viewCart['id']."'>";
                         echo "<input type='hidden' name='user_id' value='".$viewCart['user_id']."'>";
                         echo "<input type='hidden' name='quantity' value='".$viewCart['quantity']."'>";
-                        echo "<button class ='minusplus' name='operation' value='subtract'>"."-"."</button>";
+                        echo "<button class ='view_cart-minusplus' name='operation' value='subtract'>"."-"."</button>";
                         echo "<span class ='count'>".$viewCart['quantity']."</span>";
-                        echo "<button class ='minusplus' name='operation' value='add'>"."+"."</button>";
+                        echo "<button class ='view_cart-minusplus' name='operation' value='add'>"."+"."</button>";
                     echo "</div>";
                 echo "</form>";
 
-                echo "<div class = total-price-item>";
+                echo "<div class = 'total-price-item'>";
                 $totalPriceItem =$viewCart['quantity'] * $viewCart['price'];
                     echo "$".$totalPriceItem;
                 echo "</div>";
 
                 echo "<div class = 'cart-item-delete'>";
                     echo "<form action='delete_item.php' method='GET'>";
-                    echo "<button class='button' type='submit' name='Delete' value='Delete'>";
+                    echo "<button class='view_cart-button' type='submit' name='Delete' value='Delete'>";
                     echo "<input type='hidden' name='item_id' value='".$viewCart['id']."'>";
                     echo "<input type='hidden' name='user_id' value='".$viewCart['user_id']."'>";
                     echo "<img src='https://clipground.com/images/x-image-png-6.png' height='35px' width='35px' alt='delete'>";
@@ -310,23 +189,23 @@ if (!isset($_SESSION['username'])) {
 
             echo "</div>";
     } 
-            echo "<div class='separator'>";
+            echo "<div class='view_cart-separator'>";
                 echo "Subtotal: $".$subTotal;
             echo "</div>";
 
-            echo "<div class ='bottom'>";
+            echo "<div class ='view_cart-bottom'>";
 
                 echo "<form action='checkout.php' method='GET'>";
                     mysqli_data_seek($getuserCart, 0); //This goes to the very first row
                     while($viewCart = mysqli_fetch_assoc($getuserCart)){
                         echo "<input type ='hidden' name='item_id' value='".$viewCart['id']."'>";
-                        echo "<input type='hidden' name='user_id' value='".$viewCart['price']."'>";
+                        echo "<input type ='hidden' name='user_id' value='".$viewCart['price']."'>";
                     }
-                    echo "<button class='button-bottom' value='checkout'>Checkout";
+                    echo "<button class='view_cart-button-bottom' value='checkout'>Checkout";
                 echo "</form>";
 
-                echo "<a class ='link-design' href='index.php'>";
-                    echo "<button class='button-bottom'>Continue Shopping";
+                echo "<a class ='link-design-view_cart' href='index.php'>";
+                    echo "<button class='view_cart-button-bottom'>Continue Shopping";
                 echo "</a>";
 
             echo "</div>";
